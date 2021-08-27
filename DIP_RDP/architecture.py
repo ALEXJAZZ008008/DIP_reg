fixed bugs in loss added lookahead settled on final form

DIP_RDP/architecture.py
@@ -4,7 +4,7 @@
 
 
 import tensorflow as tf
-# import tensorflow_addons as tfa
+import tensorflow_addons as tfa
 import tensorflow.keras as k
 from tqdm import trange
 
@@ -30,27 +30,15 @@ def get_input(input_shape):
 def get_encoder(x, kernel_regularisation, sparseness):
     print("get_encoder")
 
-    layer_layers = [2, 2, 2, 2]
-    layer_depth = [4, 8, 16, 32]
-    layer_kernel_size = [(3, 3, 3), (3, 3, 3), (3, 3, 3), (3, 3, 3)]
-    layer_stride = [(2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)]
-    layer_groups = [2, 2, 2, 2]
+    layer_layers = [2, 2, 2, 2, 2]
+    layer_depth = [2, 4, 8, 16, 32]
+    layer_kernel_size = [(3, 3, 3), (3, 3, 3), (3, 3, 3), (3, 3, 3), (3, 3, 3)]
+    layer_stride = [(2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)]
+    layer_groups = [1, 2, 2, 2, 2]
 
     unet_connections = []
 
     x = k.layers.GaussianNoise(parameters.gaussian_sigma)(x)
-
-    x = k.layers.Conv3D(filters=layer_depth[0],
-                        kernel_size=(1, 1, 1),
-                        strides=(1, 1, 1),
-                        dilation_rate=(1, 1, 1),
-                        groups=1,
-                        padding="valid",
-                        kernel_initializer="he_normal",
-                        bias_initializer=k.initializers.Constant(0.0))(x)
-    x = k.layers.BatchNormalization()(x)
-    x = k.layers.PReLU(alpha_initializer=k.initializers.Constant(0.3),
-                       shared_axes=[1, 2, 3])(x)
 
     # layer 1
     for i in trange(len(layer_layers)):
@@ -86,10 +74,10 @@ def get_latent(x, kernel_regularisation, sparseness):
                             kernel_initializer="he_normal",
                             bias_initializer=k.initializers.Constant(0.0),
                             name="latent")(x)
+        x = k.layers.Lambda(layers.channel_shuffle, arguments={"groups": layer_groups[i]})(x)
         x = k.layers.BatchNormalization()(x)
         x = k.layers.PReLU(alpha_initializer=k.initializers.Constant(0.3),
                            shared_axes=[1, 2, 3])(x)
-        x = k.layers.Lambda(layers.channel_shuffle, arguments={"groups": layer_groups[i]})(x)
 
         x = k.layers.GaussianNoise(parameters.gaussian_sigma)(x)
 
@@ -103,11 +91,11 @@ def get_latent(x, kernel_regularisation, sparseness):
 def get_decoder(x, unet_connections, kernel_regularisation, sparseness):
     print("get_decoder")
 
-    layer_layers = [2, 2, 2, 2]
-    layer_depth = [32, 16, 8, 4]
-    layer_kernel_size = [(3, 3, 3), (3, 3, 3), (3, 3, 3), (3, 3, 3)]
-    layer_stride = [(2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)]
-    layer_groups = [2, 2, 2, 2]
+    layer_layers = [2, 2, 2, 2, 2]
+    layer_depth = [32, 16, 8, 4, 2]
+    layer_kernel_size = [(3, 3, 3), (3, 3, 3), (3, 3, 3), (3, 3, 3), (3, 3, 3)]
+    layer_stride = [(2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2), (2, 2, 2)]
+    layer_groups = [2, 2, 2, 2, 1]
 
     for i in trange(len(layer_depth)):
         x = layers.get_transpose_convolution_layer(x, layer_depth[i], layer_kernel_size[i], (1, 1, 1), layer_groups[i],
@@ -161,14 +149,12 @@ def get_model(input_shape):
 
     model = k.Model(inputs=input_x, outputs=[output_x])
 
-    # optimiser = tfa.optimizers.extend_with_decoupled_weight_decay(tf.keras.optimizers.Nadam)\
-    #     (weight_decay=0.0, learning_rate=1e-02, clipvalue=1.0) # noqa
-
-    optimiser = tf.optimizers.Nadam(learning_rate=1e-03, clipvalue=1.0)
+    optimiser = tfa.optimizers.Lookahead(tfa.optimizers.extend_with_decoupled_weight_decay(tf.keras.optimizers.Nadam)
+                                         (weight_decay=1e-03, learning_rate=1e-03, clipvalue=1.0), # noqa
+                                         sync_period=6, slow_step_size=0.5, clipvalue=1.0)
 
     if parameters.relative_difference_bool:
-        # loss = losses.log_cosh_total_variation_loss
-        loss = losses.log_cosh_loss
+        loss = losses.log_cosh_relative_difference_loss
     else:
         loss = losses.log_cosh_loss
 

DIP_RDP/losses.py
@@ -24,7 +24,7 @@ def log_cosh_loss(y_true, y_pred):
     return tf.math.reduce_mean(_log_cosh(y_pred - y_true))
 
 
-def total_variation_loss(_, y_pred):
+def total_variation_loss(y_true, y_pred):
     def _pixel_dif_one_distance(n):
         return 1.0 / tf.cast(n, dtype=tf.float32)
 
@@ -98,7 +98,7 @@ def log_cosh_total_variation_loss(y_true, y_pred):
     return log_cosh_loss(y_true, y_pred) + (0.0 * total_variation_loss(y_true, y_pred))
 
 
-def relative_difference_loss(_, y_pred):
+def relative_difference_loss(y_true, y_pred):
     def _pixel_dif_one_distance(n):
         return 1.0 / tf.cast(n, dtype=tf.float32)
 
@@ -210,7 +210,7 @@ def relative_difference_loss(_, y_pred):
 
 
 def log_cosh_relative_difference_loss(y_true, y_pred):
-    return log_cosh_loss(y_true, y_pred) + (1e01 * relative_difference_loss(y_true, y_pred))
+    return log_cosh_loss(y_true, y_pred) + (0.0 * relative_difference_loss(y_true, y_pred))
 
 
 def correlation_coefficient_loss(y_true, y_pred):

DIP_RDP/main.py
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt
 import nibabel as nib
 from tqdm import trange
 
+import losses
 
 reproducible_bool = True
 
@@ -172,7 +173,7 @@ def get_train_data():
             current_shape = current_array[0].shape
 
         current_y_train_path = "{0}/{1}.npy".format(y_train_output_path, str(i))
-        np.save(current_y_train_path, current_array, allow_pickle=True, fix_imports=False)
+        np.save(current_y_train_path, current_array)
         y.append(current_y_train_path)
 
         if gaussian_noise is None:
@@ -191,7 +192,7 @@ def get_train_data():
         current_array = (current_array + (gaussian_weight * gaussian_noise)) / (1.0 + gaussian_weight)
 
         current_x_train_path = "{0}/{1}.npy".format(x_train_output_path, str(i))
-        np.save(current_x_train_path, current_array, allow_pickle=True, fix_imports=False)
+        np.save(current_x_train_path, current_array)
         x.append(current_x_train_path)
 
     y = np.asarray(y)
@@ -216,7 +217,7 @@ def get_train_data():
             current_array, _ = get_data_windows(current_array)
 
             current_gt_train_path = "{0}/{1}.npy".format(gt_train_output_path, str(i))
-            np.save(current_gt_train_path, current_array, allow_pickle=True, fix_imports=False)
+            np.save(current_gt_train_path, current_array)
             gt.append(current_gt_train_path)
 
         gt = np.asarray(gt)
@@ -288,6 +289,84 @@ def get_model_memory_usage(batch_size, model):
     gbytes = np.round(total_memory / (1024.0 ** 3), 3) + internal_model_mem_count
 
     return gbytes
+
+
+def train_step(model, optimiser, loss, x_train_iteration, y_train_iteration, loss_array, previous_model_weight_array,
+               relative_plateau_cutoff, model_output_path, iteration):
+    current_loss_increase_patience = 0
+
+    while True:
+        previous_model_weight_array.append("{0}/{1}.pkl".format(model_output_path, str(iteration)))
+        pickle.dump(model.get_weights(), open(previous_model_weight_array[-1], "wb"))
+
+        # Open a GradientTape to record the operations run
+        # during the forward pass, which enables auto-differentiation.
+        with tf.GradientTape() as tape:
+
+            # Run the forward pass of the layer.
+            # The operations that the layer applies
+            # to its inputs are going to be recorded
+            # on the GradientTape.
+            logits = model(x_train_iteration, training=True)  # Logits for this minibatch
+
+            # Compute the loss value for this minibatch.
+            current_loss = loss(y_train_iteration, logits)
+            current_loss = current_loss + np.mean(model.losses[::3])
+            current_loss = current_loss + np.mean([model.losses[1::3], model.losses[2::3]])
+
+        loss_array.append(current_loss)
+
+        if relative_plateau_cutoff is None:
+            relative_plateau_cutoff = parameters.plateau_cutoff * current_loss
+
+        if len(loss_array) > 1:
+            loss_gradient = np.gradient(loss_array)[-1]
+
+            if not np.allclose(loss_gradient, np.zeros(loss_gradient.shape), atol=relative_plateau_cutoff):
+                if current_loss > loss_array[-1]:
+                    if current_loss_increase_patience >= parameters.patience:
+                        print("WARNING Loss increased; patience reached, allowing anyway!")
+
+                        break
+                    else:
+                        print("WARNING Loss increased; backing up...")
+
+                        model.set_weights(pickle.load(open(previous_model_weight_array[-1], "rb")))
+
+                        previous_model_weight_array.pop()
+                        loss_array.pop()
+
+                        if current_loss_increase_patience > 1:
+                            previous_model_weight_array.pop()
+                            loss_array.pop()
+
+                            iteration = iteration - 1
+
+                        current_loss_increase_patience = current_loss_increase_patience + 1
+                else:
+                    break
+            else:
+                break
+        else:
+            break
+
+    # Use the gradient tape to automatically retrieve
+    # the gradients of the trainable variables with respect to the loss.
+    grads = tape.gradient(current_loss, model.trainable_weights)
+
+    # Run one step of gradient descent by updating
+    # the value of the variables to minimize the loss.
+    optimiser.apply_gradients(zip(grads, model.trainable_weights))
+
+    return model, loss_array, previous_model_weight_array, relative_plateau_cutoff, iteration
+
+
+def test_step(model, x_train_iteration, y_train_iteration):
+    x_prediction = np.squeeze(model(x_train_iteration, training=False))
+
+    current_accuracy = get_evaluation_score(x_prediction, y_train_iteration)
+
+    return x_prediction, current_accuracy
 
 
 def get_evaluation_score(prediction, gt):
@@ -368,7 +447,7 @@ def train_model():
     x, y, preprocessing_steps, full_input_shape, windowed_full_input_axial_size, window_input_shape, gt = \
         get_preprocessed_train_data()
 
-    data_shape = np.load(x[0], mmap_mode="r+", allow_pickle=True)[0].shape
+    data_shape = np.load(x[0])[0].shape
 
     model, optimiser, loss = architecture.get_model(data_shape)
     model.summary()
@@ -387,7 +466,7 @@ def train_model():
         if not os.path.exists(patient_output_path):
             os.makedirs(patient_output_path, mode=0o770)
 
-        number_of_windows = np.load(x[i], mmap_mode="r+", allow_pickle=True).shape[0]
+        number_of_windows = np.load(x[i]).shape[0]
 
         current_window_data_paths = []
 
@@ -412,8 +491,8 @@ def train_model():
             if not os.path.exists(model_output_path):
                 os.makedirs(model_output_path, mode=0o770)
 
-            x_train_iteration = np.asarray([np.load(x[i], mmap_mode="r+", allow_pickle=True)[j]])
-            y_train_iteration = np.asarray([np.load(y[i], mmap_mode="r+", allow_pickle=True)[j]])
+            x_train_iteration = np.asarray([np.load(x[i])[j]])
+            y_train_iteration = np.asarray([np.load(y[i])[j]])
 
             if float_sixteen_bool:
                 x_train_iteration = x_train_iteration.astype(np.float16)
@@ -423,7 +502,7 @@ def train_model():
                 y_train_iteration = y_train_iteration.astype(np.float32)
 
             if gt is not None:
-                gt_prediction = np.asarray([np.load(gt[i], mmap_mode="r+", allow_pickle=True)[j]])
+                gt_prediction = np.asarray([np.load(gt[i])[j]])
             else:
                 gt_prediction = None
 
@@ -435,74 +514,13 @@ def train_model():
             while True:
                 print("Iteration:\t{0}".format(str(iteration)))
 
-                current_loss_increase_patience = 0
+                model, loss_array, previous_model_weight_array, relative_plateau_cutoff, iteration = \
+                    train_step(model, optimiser, loss, x_train_iteration, y_train_iteration, loss_array,
+                               previous_model_weight_array, relative_plateau_cutoff, model_output_path, iteration)
 
-                while True:
-                    previous_model_weight_array.append("{0}/{1}.pkl".format(model_output_path, str(iteration)))
-                    pickle.dump(model.get_weights(), open(previous_model_weight_array[-1], "wb"))
+                x_prediction, current_accuracy = test_step(model, x_train_iteration, y_train_iteration)
 
-                    # Open a GradientTape to record the operations run
-                    # during the forward pass, which enables auto-differentiation.
-                    with tf.GradientTape() as tape:
-
-                        # Run the forward pass of the layer.
-                        # The operations that the layer applies
-                        # to its inputs are going to be recorded
-                        # on the GradientTape.
-                        logits = model(x_train_iteration, training=True)  # Logits for this minibatch
-
-                        # Compute the loss value for this minibatch.
-                        current_loss = loss(y_train_iteration, logits)
-
-                    loss_array.append(current_loss)
-
-                    if relative_plateau_cutoff is None:
-                        relative_plateau_cutoff = parameters.plateau_cutoff * current_loss
-
-                    if len(loss_array) > 1:
-                        loss_gradient = np.gradient(loss_array)[-1]
-
-                        if not np.allclose(loss_gradient, np.zeros(loss_gradient.shape), atol=relative_plateau_cutoff):
-                            if current_loss > loss_array[-1]:
-                                if current_loss_increase_patience >= parameters.patience:
-                                    print("WARNING Loss increased; patience reached, allowing anyway!")
-
-                                    break
-                                else:
-                                    print("WARNING Loss increased; backing up...")
-
-                                    model.set_weights(pickle.load(open(previous_model_weight_array[-1], "rb")))
-
-                                    previous_model_weight_array.pop()
-                                    loss_array.pop()
-
-                                    if current_loss_increase_patience > 1:
-                                        previous_model_weight_array.pop()
-                                        loss_array.pop()
-
-                                        iteration = iteration - 1
-
-                                    current_loss_increase_patience = current_loss_increase_patience + 1
-                            else:
-                                break
-                        else:
-                            break
-                    else:
-                        break
-
-                # Use the gradient tape to automatically retrieve
-                # the gradients of the trainable variables with respect to the loss.
-                grads = tape.gradient(current_loss, model.trainable_weights)
-
-                # Run one step of gradient descent by updating
-                # the value of the variables to minimize the loss.
-                optimiser.apply_gradients(zip(grads, model.trainable_weights))
-
-                x_prediction = np.squeeze(model(x_train_iteration, training=False))
-
-                current_accuracy = get_evaluation_score(x_prediction, y_train_iteration)
-
-                print("Loss:\t{0:<20}\tAccuracy:\t{1:<20}".format(str(float(current_loss)), str(current_accuracy)))
+                print("Loss:\t{0:<20}\tAccuracy:\t{1:<20}".format(str(float(loss_array[-1])), str(current_accuracy)))
 
                 if gt is not None:
                     gt_accuracy = get_evaluation_score(x_prediction, gt_prediction)

DIP_RDP/preprocessing.py
@@ -35,7 +35,7 @@ def data_upsample(data, data_type, new_resolution=None):
 
     for i in trange(len(data)):
         if data_type == "path":
-            data_copy = np.load(data[i], mmap_mode="r+", allow_pickle=True)
+            data_copy = np.load(data[i])
         else:
             if data_type == "numpy":
                 data_copy = data[i].copy()
@@ -77,7 +77,7 @@ def data_upsample(data, data_type, new_resolution=None):
         data_copy = np.expand_dims(data_copy, -1)
 
         if data_type == "path":
-            np.save(data[i], data_copy, allow_pickle=True, fix_imports=False)
+            np.save(data[i], data_copy)
         else:
             if data_type == "numpy":
                 data[i] = data_copy
@@ -130,7 +130,7 @@ def data_preprocessing(data, data_type, preprocessing_steps=None):
 
     for i in trange(len(data)):
         if data_type == "path":
-            data_copy = np.load(data[i], mmap_mode="r+", allow_pickle=True)
+            data_copy = np.load(data[i])
         else:
             if data_type == "numpy":
                 data_copy = data[i].copy()
@@ -163,7 +163,7 @@ def data_preprocessing(data, data_type, preprocessing_steps=None):
         data_copy = data_copy.reshape(data_copy_shape)
 
         if data_type == "path":
-            np.save(data[i], data_copy, allow_pickle=True, fix_imports=False)
+            np.save(data[i], data_copy)
         else:
             if data_type == "numpy":
                 data[i] = data_copy
@@ -176,7 +176,7 @@ def redistribute(data, data_type, robust_bool=False, new_distribution=None, new_
 
     for i in trange(len(data)):
         if data_type == "path":
-            data_copy = np.load(data[i], mmap_mode="r+", allow_pickle=True)
+            data_copy = np.load(data[i])
         else:
             if data_type == "numpy":
                 data_copy = data[i].copy()
@@ -193,7 +193,7 @@ def redistribute(data, data_type, robust_bool=False, new_distribution=None, new_
 
         if new_distribution is not None:
             if new_distribution_type == "path":
-                new_distribution_copy = np.load(new_distribution[i], mmap_mode="r+", allow_pickle=True)
+                new_distribution_copy = np.load(new_distribution[i])
             else:
                 if new_distribution_type == "numpy":
                     new_distribution_copy = new_distribution[i].copy()
@@ -213,7 +213,7 @@ def redistribute(data, data_type, robust_bool=False, new_distribution=None, new_
         data_copy = data_copy.reshape(data_copy_shape)
 
         if data_type == "path":
-            np.save(data[i], data_copy, allow_pickle=True, fix_imports=False)
+            np.save(data[i], data_copy)
         else:
             if data_type == "numpy":
                 data[i] = data_copy